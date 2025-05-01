943a8d85769e635dbadde181e6f073061b475eb1
sergiram <sergiram94@gmail.com>
2025-05-01 20:54:30 +0200

    Agregada funcionalidad

--- src/App.tsx
+++ src/App.tsx
@@ -11,57 +11,85 @@ console.log(import.meta.env);
 console.log("API KEY: ", API_KEY);
 
 function App() {
-  const [videoId, setVideoId] = useState<string>("");
+  const [videoUrl, setVideoUrl] = useState<string>("");
   const [comments, setComments] = useState<Comment[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
 
+  function extractVideoId(url: string):string|null {
+    try{
+      const parsedUrl = new URL(url);
+      const videoId = parsedUrl.searchParams.get("v");
+      return videoId;
+    }catch{
+      return null;
+    }
+  }
+
   const fetchComments = async () => {
-    // if(!videoId) return;
-    
-    // setLoading(true);
-    // setError('');
-    // setComments([]);
+
+    const videoId = extractVideoId(videoUrl);
+
+    if(!videoId) return;
+
+    setLoading(true);
+    setError('');
+    setComments([]);
     console.log('entra')
     try{
-      // const response = await fetch(
-        
-      // // );
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&key=${API_KEY}&maxResults=10`
+      );
 
-      // console.log('Respuesta API: ', response);
-      // const data = await response.json();
-      // console.log('fetchComments Respuesta API: ', data);
-      // if(data.error){
-      //   setError(data.error);
-      //   setLoading(false);
-      //   return;
-      // }
+      console.log('Respuesta API: ', response);
+      const data = await response.json();
+      console.log('fetchComments Respuesta API: ', data);
+      if(data.error){
+        setError(data.error.message);
+        setLoading(false);
+        return;
+      }
 
-      // const commentsData: Comment[] = data.
+      const commentData: Comment[] = data.items.map((item: any) => {
+        return{
+          author: item.snippet.topLevelComment.snippet.authorDisplayName,
+          text: item.snippet.topLevelComment.snippet.textDisplay
+        };
+      })
 
-    }catch(err){
+      setComments(commentData);
 
+    }catch(err: any){
+      console.error("Error: ", err);
+      setError(err?.message || "Error desconocido.");
+    }finally{
+      setLoading(false);
     }
 
 
   }
 
-  useEffect(() => {
-    if(videoId){
-      fetchComments();
-    }
-  }, [videoId])
-  
-
   return (
     <div className="App">
       <h1>Comentarios YT</h1>
-      <input 
+      <input
         type='text'
-        placeholder='Introduce video ID'
-        value={videoId}
-        onChange={(e) => setVideoId(e.target.value)}
+        placeholder='Introduce video Url...'
+        value={videoUrl}
+        onChange={(e) => setVideoUrl(e.target.value)}
       />
+
+      <button onClick={fetchComments}>Buscar</button>
+
+      {loading && <p>Cargando comentarios...</p>}
+      {error && <p className="text-red-500">{error}</p>}
+      <ul>
+        {comments.map((comment,index) => (
+          <li key={index}>
+            {comment.author}: {comment.text}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
